ef16e094e1e37b2f93db9f3763bad3a629a0c2f8
Roger Lo <sobadrush@icloud.com>
2025-08-08 22:43:48 +0800

    [feat](遊戲): 新增貪食蛇初始變數
    - 新增貪食蛇的初始位置 snake_pos
    
    - 新增貪食蛇的初始身體 snake_body
    
    - 新增貪食蛇的初始方向 direction

--- snake.py
+++ snake.py
@@ -26,6 +26,12 @@ GREEN = pygame.Color(0, 255, 0)
 clock = pygame.time.Clock()
 snake_speed = 15
 
+# 遊戲變數
+snake_pos = [100, 60]
+snake_body = [[100, 60], [80, 60], [60, 60]]
+direction = 'RIGHT'
+change_to = direction
+
 while True:
   
   # 這幾行註解掉看看
@@ -33,7 +39,10 @@ while True:
     if event.type == pygame.QUIT:
       pygame.quit()
       sys.exit()
-  
+      
+    # 遊戲邏輯更新 (暫時為空)
+    
+  # 畫面渲染
   screen.fill(BLACK)
   pygame.display.update()
   clock.tick(snake_speed)
